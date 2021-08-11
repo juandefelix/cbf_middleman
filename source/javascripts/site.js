76435c9d92d74b069c507e27f946640cd9bfa227
Jordan <jordan.w.christianson@gmail.com>
2021-08-11 16:23:12 -0500

    Update package-lock.json and fix popover functionality

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,5 @@
 
+
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
@@ -6,7 +7,8 @@ var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
 
-
+/*
 var popover = new bootstrap.Popover(document.querySelector('[data-bs-toggle="popover"]'), {
   container: 'body'
 })
+*/

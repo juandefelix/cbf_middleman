4c953bb81b55f3a43dbf084a33f2211e226acaee
Jordan <jordan.w.christianson@gmail.com>
2021-09-20 10:16:36 -0500

    Edit content and add images for several pages

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,11 +1,11 @@
 
-
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
 
 /*
 var popover = new bootstrap.Popover(document.querySelector('[data-bs-toggle="popover"]'), {
